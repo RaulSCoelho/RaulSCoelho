0d7c8b3a152a27ee8be6460d428bacf75ddf92d2
RaulSCoelho <raul.s.coelho@gmail.com>
2024-06-09 01:52:25 -0300

    Tailwind CSS scrollbar plugin

--- packages/tailwind/tailwind-scrollbar/CHANGELOG.md
+++ packages/tailwind/tailwind-scrollbar/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @raulscoelho/tailwind-scrollbar
 
+## 1.0.2
+
+### Patch Changes
+
+- Tailwind CSS scrollbar plugin
+
 ## 1.0.1
 
 ### Patch Changes
---
+++ packages/tailwind/tailwind-scrollbar/src/plugin.ts
@@ -0,0 +1,174 @@
+import deepMerge from 'deepmerge'
+import plugin from 'tailwindcss/plugin'
+const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette");
+
+import { Colors, Layout, defaultColors, defaultLayout, flattenThemeObject, getColorVariables } from './utils'
+import mapKeys from 'lodash.mapkeys'
+import kebabCase from 'lodash.kebabcase'
+
+const DEFAULT_PREFIX = 'scrollbar'
+
+interface Config {
+  colors?: Colors
+  layout?: Layout
+}
+
+// @internal
+function resolveConfig({ colors, layout }: Config, prefix: string) {
+  const resolved: {
+    layout: Record<string, any>
+    colors: Record<string, any>
+  } = {
+    colors: {},
+    layout: {}
+  }
+  const cssSelector = ':root'
+  const flatColors = flattenThemeObject(colors) as Record<string, string>
+  const flatLayout = layout ? mapKeys(layout, (value, key) => kebabCase(key)) : {}
+
+  resolved.colors[cssSelector] = {}
+  resolved.layout[cssSelector] = {}
+
+  /**
+   * Colors
+   */
+  for (const colorsEntries of Object.entries(flatColors)) {
+    const colorName = kebabCase(colorsEntries[0])
+    const colorValue = colorsEntries[1]
+
+    if (!colorValue) return
+
+    try {
+      const { hsl, defaultAlphaValue, scrollbarColorVariable } = getColorVariables(colorName, colorValue, prefix)
+
+      // set the css variable in "@layer utilities"
+      resolved.colors[cssSelector]![scrollbarColorVariable] = `hsl(${hsl})`
+
+      // if an alpha value was provided in the color definition, store it in a css variable
+      if (typeof defaultAlphaValue === 'number') {
+        resolved.colors[cssSelector]![scrollbarColorVariable] = `hsl(${hsl} / ${defaultAlphaValue.toFixed(2)})`
+      }
+    } catch (error: any) {
+      console.log('error', error?.message)
+    }
+  }
+
+  /**
+   * Layout
+   */
+  for (const [key, value] of Object.entries(flatLayout)) {
+    if (!value) return
+
+    const layoutVariablePrefix = `--${prefix}-${key}`
+
+    if (typeof value === 'object') {
+      for (const [nestedKey, nestedValue] of Object.entries(value)) {
+        const nestedLayoutVariable = `${layoutVariablePrefix}-${nestedKey}`
+
+        resolved.layout[cssSelector]![nestedLayoutVariable] = nestedValue
+      }
+    } else {
+      // Handle opacity values and other singular layout values
+      const formattedValue =
+        layoutVariablePrefix.includes('opacity') && typeof value === 'number'
+          ? value.toString().replace(/^0\./, '.')
+          : value
+
+      resolved.layout[cssSelector]![layoutVariablePrefix] = formattedValue
+    }
+  }
+
+  return resolved
+}
+
+export interface TailwindScrollbarConfig {
+  colors?: Colors
+  layout?: Layout
+  prefix?: string
+}
+
+export function tailwindScrollbar({
+  colors = defaultColors,
+  layout = defaultLayout,
+  prefix = DEFAULT_PREFIX
+}: TailwindScrollbarConfig = {}) {
+  const resolved = resolveConfig({ colors, layout }, prefix)
+  const resolvedColors = resolved?.colors ?? {}
+  const resolvedLayout = resolved?.layout ?? {}
+  const utilities = deepMerge(resolvedColors, resolvedLayout)
+
+  return plugin(({ addBase, addUtilities, matchUtilities, theme }) => {
+    addBase({
+      '::-webkit-scrollbar': {
+        width: `var(--${prefix}-width)`,
+        height: `var(--${prefix}-height)`
+      },
+
+      '::-webkit-scrollbar-thumb': {
+        backgroundColor: `var(--${prefix}-thumb) !important`,
+        borderRadius: `var(--${prefix}-thumb-radius) !important`,
+        border: `0.18rem solid var(--${prefix}-track) !important`
+      },
+
+      '::-webkit-scrollbar-thumb:hover': {
+        backgroundColor: `var(--${prefix}-thumb-hover) !important`
+      },
+
+      '::-webkit-scrollbar-thumb:active': {
+        backgroundColor: `var(--${prefix}-thumb-active) !important`
+      },
+
+      '::-webkit-scrollbar-track': {
+        backgroundColor: `var(--${prefix}-track) !important`,
+        borderRadius: `var(--${prefix}-track-radius) !important`
+      }
+    })
+
+    // add the css variables to "@layer utilities"
+    addUtilities(utilities)
+
+    matchUtilities(
+      {
+        [`${prefix}-w`]: value => ({
+          [`--${prefix}-width`]: value
+        }),
+        [`${prefix}-h`]: value => ({
+          [`--${prefix}-height`]: value
+        })
+      },
+      {
+        values: theme('spacing')
+      }
+    )
+
+    matchUtilities(
+      {
+        [`${prefix}-thumb-rounded`]: value => ({
+          [`--${prefix}-thumb-radius`]: value
+        }),
+        [`${prefix}-track-rounded`]: value => ({
+          [`--${prefix}-track-radius`]: value
+        })
+      },
+      {
+        values: theme('borderRadius')
+      }
+    )
+
+    matchUtilities(
+      Object.keys(resolvedColors[':root'] ?? {}).reduce(
+        (acc, key) => {
+          acc[key.slice(2)] = (value: any) => ({
+            [key]: typeof value === 'function' ? value({}) : value
+          })
+          return acc
+        },
+        {} as Record<string, any>
+      ),
+      {
+        type: 'color',
+        values: flattenColorPalette(theme('colors'))
+      }
+    )
+  })
+}
